328fd615602c5eb586e4d3ae4f1e1077aa1e81ff
Ernesto Barreras <ernesto_barreras@manulife.com>
2018-03-01 15:38:25 -0500

    Product item component, display start rating using Font Awesome

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'nga-stars',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarsComponent implements OnInit {
 
+  @Input() count = 5
+  @Input() rating = 0
+  stars: boolean[] = []
+
   constructor() { }
 
   ngOnInit() {
+    this.stars = Array.from(Array(this.count).keys()).map(i => i < Math.round(this.rating))
   }
 
 }
